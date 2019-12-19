3ad458216feb7a142bf750a8a79aa6cbbac736b9
ArielLB <arielelbachar@gmail.com>
2019-12-19 13:09:35 +0200

    added the get options and added option for admins to edit the dropdowns and a few minor fixes and deployed into heroku closes #28 #29

--- routes/sensorCreateOptions.js
+++ routes/sensorCreateOptions.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { createOption, getOptions } = require("../controllers/sensorCreateOptions");
+const { createOptionCollection, getOptions, editOption } = require("../controllers/sensorCreateOptions");
 
-router.post("/create-input-options", createOption);
+// add admin middleware
+router.post("/create-input-options", createOptionCollection);
+router.post("/edit-dropdown", editOption);
+//
+
 router.get("/print-inputs-options", getOptions);
 
 module.exports = router;
